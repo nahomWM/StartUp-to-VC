d9517a5caccab2a949ff6d3a4d878a20be74b342
Nahom Wm <dbu1601542@dbu.edu.et>
2025-06-30 13:45:00 +0300

    Add deleteVC route with protection

--- src/routes/vcRoutes.js
+++ src/routes/vcRoutes.js
@@ -7,3 +7,4 @@ const router = express.Router();
 router.get('/', vcController.getAllVCs);
 router.get('/:id', vcController.getVC);
 router.patch('/:id', authMiddleware.protect, vcController.updateVC);
+router.delete('/:id', authMiddleware.protect, vcController.deleteVC);

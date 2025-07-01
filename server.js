95249ddb870d67133f566ac113a7e6d7f31afa85
Nahom Wm <dbu1601542@dbu.edu.et>
2025-07-01 09:10:00 +0300

    Initialize server.js with basic express setup

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const config = require('./src/config/environment');
+const connectDB = require('./src/config/database');
+const errorHandler = require('./src/middleware/errorHandler');
+
+const app = express();
+
+// Connect to database
+connectDB();
+
+app.get('/', (req, res) => {
+    res.send('Star-Bust API is running...');
+});
+
+app.use(errorHandler);
+
+const PORT = config.port || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running in ${config.env} mode on port ${PORT}`);
+});

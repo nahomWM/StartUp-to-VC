89afff3a290c3d5999cb1808ab4b1da7c439ca66
Nahom Wm <dbu1601542@dbu.edu.et>
2025-07-04 16:15:00 +0300

    Add getAdminById method to Admin service

--- src/services/adminService.js
+++ src/services/adminService.js
@@ -5,3 +5,11 @@ exports.getAllAdmins = async () => {
     const admins = await Admin.find();
     return admins;
 };
+
+exports.getAdminById = async (id) => {
+    const admin = await Admin.findById(id);
+    if (!admin) {
+        throw new AppError('No admin found with that ID', 404);
+    }
+    return admin;
+};

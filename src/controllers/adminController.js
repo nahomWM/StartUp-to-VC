590cd775af97bcf132cfe41abf606d89b3ab2cfa
Nahom Wm <dbu1601542@dbu.edu.et>
2025-07-11 16:20:00 +0300

    Add updateMe and deleteMe methods to Admin controller

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -54,3 +54,23 @@ exports.getMe = catchAsync(async (req, res, next) => {
         }
     });
 });
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+    const admin = await adminService.updateMe(req.user.id, req.body);
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            admin
+        }
+    });
+});
+
+exports.deleteMe = catchAsync(async (req, res, next) => {
+    await adminService.deleteMe(req.user.id);
+
+    res.status(204).json({
+        status: 'success',
+        data: null
+    });
+});

07e1d998b2957845c1d83c0e1048886ee3665868
Nahom Wm <dbu1601542@dbu.edu.et>
2025-07-12 09:30:00 +0300

    Implement VC registration and update validators

--- src/validators/vcValidator.js
+++ src/validators/vcValidator.js
@@ -1,7 +1,15 @@
 const { body } = require('express-validator');
 
+exports.registerVCValidator = [
+    body('type').isIn(['individual', 'organization']).withMessage('Invalid VC type'),
+    body('profile.firstName').if(body('type').equals('individual')).notEmpty().withMessage('First name is required for individuals'),
+    body('profile.lastName').if(body('type').equals('individual')).notEmpty().withMessage('Last name is required for individuals'),
+    body('profile.firmName').if(body('type').equals('organization')).notEmpty().withMessage('Firm name is required for organizations'),
+    body('profile.phone').notEmpty().withMessage('Phone number is required'),
+    body('location.country').notEmpty().withMessage('Country is required')
+];
+
 exports.updateVCValidator = [
-    body('profile.firmName').optional().notEmpty().withMessage('Firm name cannot be empty'),
     body('profile.phone').optional().notEmpty().withMessage('Phone number cannot be empty'),
     body('location.country').optional().notEmpty().withMessage('Country cannot be empty'),
     body('investmentCriteria.industries').optional().isArray().withMessage('Industries must be an array'),

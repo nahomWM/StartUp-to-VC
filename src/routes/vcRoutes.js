c628523bb9aa24464c62d041932f7aaa6f0a03ea
Nahom Wm <dbu1601542@dbu.edu.et>
2025-07-12 11:00:00 +0300

    Integrate validators into VC routes

--- src/routes/vcRoutes.js
+++ src/routes/vcRoutes.js
@@ -2,17 +2,19 @@ const express = require('express');
 const vcController = require('../controllers/vcController');
 const authMiddleware = require('../middleware/auth');
 
+const vcValidator = require('../validators/vcValidator');
+
 const router = express.Router();
 
 router.use(authMiddleware.protect);
 
 router.get('/me', vcController.getMe);
-router.patch('/updateMe', vcController.updateMe);
+router.patch('/updateMe', vcValidator.updateVCValidator, vcController.updateMe);
 router.delete('/deleteMe', vcController.deleteMe);
 
 router.get('/', vcController.getAllVCs);
 router.get('/:id', vcController.getVC);
-router.patch('/:id', vcController.updateVC);
+router.patch('/:id', vcValidator.updateVCValidator, vcController.updateVC);
 router.delete('/:id', vcController.deleteVC);
 
 module.exports = router;

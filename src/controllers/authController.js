2a827b2039a6141de43a15b0de2eefc8d4e8f1bd
Nahom Wm <dbu1601542@dbu.edu.et>
2025-07-16 18:45:00 +0300

    Add updateMe and deleteMe methods to Auth controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -100,3 +100,23 @@ exports.getMe = catchAsync(async (req, res, next) => {
         }
     });
 });
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+    const updatedUser = await authService.updateMe(req.user, req.body);
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            user: updatedUser
+        }
+    });
+});
+
+exports.deleteMe = catchAsync(async (req, res, next) => {
+    await authService.deleteMe(req.user);
+
+    res.status(204).json({
+        status: 'success',
+        data: null
+    });
+});

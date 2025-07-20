233626a883854c72549495fed00749bcc7708e07
Nahom Wm <dbu1601542@dbu.edu.et>
2025-07-20 09:00:00 +0300

    Implement Startup service and API features utility

---
+++ src/services/startupService.js
@@ -0,0 +1,50 @@
+const Startup = require('../models/startupModel');
+const AppError = require('../utils/AppError');
+const APIFeatures = require('../utils/apiFeatures');
+
+exports.getAllStartups = async (queryString) => {
+    const features = new APIFeatures(Startup.find(), queryString)
+        .filter()
+        .sort()
+        .limitFields()
+        .paginate();
+
+    const startups = await features.query;
+    return startups;
+};
+
+exports.getStartupById = async (id) => {
+    const startup = await Startup.findById(id);
+    if (!startup) {
+        throw new AppError('No startup found with that ID', 404);
+    }
+    return startup;
+};
+
+exports.getStartupsBySector = async (sector) => {
+    const startups = await Startup.find({ industry: sector });
+    return startups;
+};
+
+exports.updateStartup = async (id, updateData) => {
+    const startup = await Startup.findByIdAndUpdate(id, updateData, {
+        new: true,
+        runValidators: true
+    });
+
+    if (!startup) {
+        throw new AppError('No startup found with that ID', 404);
+    }
+
+    return startup;
+};
+
+exports.deleteStartup = async (id) => {
+    const startup = await Startup.findByIdAndDelete(id);
+
+    if (!startup) {
+        throw new AppError('No startup found with that ID', 404);
+    }
+
+    return startup;
+};

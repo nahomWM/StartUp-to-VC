969ddb54dc3b5f9cf3c202216d9607a37e7f23bb
Nahom Wm <dbu1601542@dbu.edu.et>
2025-07-21 11:30:00 +0300

    Add Startup controller with CRUD operations

---
+++ src/controllers/startupController.js
@@ -0,0 +1,57 @@
+const startupService = require('../services/startupService');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getAllStartups = catchAsync(async (req, res, next) => {
+    const startups = await startupService.getAllStartups(req.query);
+
+    res.status(200).json({
+        status: 'success',
+        results: startups.length,
+        data: {
+            startups
+        }
+    });
+});
+
+exports.getStartup = catchAsync(async (req, res, next) => {
+    const startup = await startupService.getStartupById(req.params.id);
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            startup
+        }
+    });
+});
+
+exports.getStartupsBySector = catchAsync(async (req, res, next) => {
+    const startups = await startupService.getStartupsBySector(req.params.sector);
+
+    res.status(200).json({
+        status: 'success',
+        results: startups.length,
+        data: {
+            startups
+        }
+    });
+});
+
+exports.updateStartup = catchAsync(async (req, res, next) => {
+    const startup = await startupService.updateStartup(req.params.id, req.body);
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            startup
+        }
+    });
+});
+
+exports.deleteStartup = catchAsync(async (req, res, next) => {
+    await startupService.deleteStartup(req.params.id);
+
+    res.status(204).json({
+        status: 'success',
+        data: null
+    });
+});
